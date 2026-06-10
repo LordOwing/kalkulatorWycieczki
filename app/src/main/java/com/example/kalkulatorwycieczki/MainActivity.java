36236d1a8bf5134b799b95e812e787d551256c10
mlolo <mlolo@technikumlodz.pl>
2026-06-10 10:33:46 +0200

    finished

--- app/src/main/java/com/example/kalkulatorwycieczki/MainActivity.java
+++ app/src/main/java/com/example/kalkulatorwycieczki/MainActivity.java
@@ -24,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
         Button buttonCalculate = findViewById(R.id.buttonCalculate);
         Button resetButton = findViewById(R.id.resetButton);
         resetButton.setOnClickListener(v -> {
-            radioGroupTransport.setSelected(false);
+            radioGroupTransport.clearCheck();
             checkBreakfast.setChecked(false);
             checkGuide.setChecked(false);
             editDays.setText("");
+            Toast.makeText(this, "Formularz został wyczyszczony!", Toast.LENGTH_SHORT).show();
                 });
         buttonCalculate.setOnClickListener(v -> {
             String daysStr = editDays.getText().toString();
@@ -37,38 +38,54 @@ public class MainActivity extends AppCompatActivity {
             }else if(Integer.parseInt(daysStr)>30 ){
                 Toast.makeText(this, "Wpisz liczbę dni mniejszą od 30!", Toast.LENGTH_SHORT).show();
                 return;
-            }else{
+            }else if(Integer.parseInt(daysStr)<1){
                 Toast.makeText(this, "Wpisz liczbę dni większą od 1: " + daysStr, Toast.LENGTH_SHORT).show();
             }
 
 
 
             int days = Integer.parseInt(daysStr);
-            int totalCost = days * 120;
-
+            int baseCost = days * 120;
+            int totalCost = baseCost;
+            int transportCost = 0;
+            String selectedTransport;
             int selectedTransportId = radioGroupTransport.getCheckedRadioButtonId();
+            String additional = "";
             if (selectedTransportId == R.id.radioOwn) {
-                totalCost += 0;
+
+                selectedTransport = "Dojazd własny";
+                transportCost = 0;
             } else if (selectedTransportId == R.id.radioBus) {
-                totalCost += 100;
+                transportCost = 100;
+                selectedTransport = "Autokar";
             } else if (selectedTransportId == R.id.radioPlane) {
-                totalCost += 500;
+                transportCost = 500;
+                selectedTransport = "Samolot";
             } else {
                 Toast.makeText(this, "Wybierz rodzaj transportu!", Toast.LENGTH_SHORT).show();
                 return;
             }
-
-            if (checkBreakfast.isChecked()) {
-                totalCost += days * 30;
-            }
-
+            totalCost += transportCost;
             if (checkGuide.isChecked()) {
                 totalCost += 150;
+                additional += "Przewodnik: (+ 150 zł) \n";
+            }
+            if (checkBreakfast.isChecked()) {
+                totalCost += days * 30;
+                additional += "Śniadania: (+ 150 zł) \n";
             }
 
+            StringBuilder summary = new StringBuilder();
+            String summary1 = "Liczba dni: " + days + "\n" +
+                    "Koszt bazowy: "+ baseCost + " zł \n" +
+                    "Transport:  "+ selectedTransport + " (+ " + transportCost + " zł )\n" ;
+            String totalText = "------------------------------------------------- \n Łącznie: " + totalCost + " zł";
+            summary.append(summary1);
+            summary.append(additional);
+            summary.append(totalText);
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Podsumowanie wycieczki");
-            builder.setMessage("Liczba dni: " + days + "\nCałkowity koszt: " + totalCost + " zł");
+            builder.setMessage(summary);
             builder.setPositiveButton("OK", null);
             builder.show();
         });
